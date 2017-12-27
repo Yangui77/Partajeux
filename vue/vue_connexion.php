bf10b76039dcebe8dcb0aaecca3b468adf49ccda
Guillaume <fuditphuguillaume@gmail.com>
2017-12-27 18:21:51 +0100

    style connexion inscription affichage des jeux de l'utilisateur

--- vue/vue_connexion.php
+++ vue/vue_connexion.php
@@ -1,9 +1,11 @@
-<form action="./index.php" method="post">
+<form action="./index.php" method="post" class="form">
+	<H1 style="text-align: center;">Connexion</H1>
         <p>
-        <label for="Idenfiant">Identifiant</label> : <input type="text" name="Identifiant" id="Identifiant" /><br />
-        <label for="Mdp">Mot de passe</label> :  <input type="text" name="Mdp" id="Mdp" /><br />
-        <br />
-
+        <label for="Idenfiant">Identifiant</label> : </br>
+        <input type="text"  name="Identifiant" id="Identifiant" /><br />
+        <label for="Mdp">Mot de passe</label> :  </br>
+        <input type="text" name="Mdp" id="Mdp"/><br />
+        <br/>
         <input type="submit" value="connexion" name="connexion" />
 	</p>
 </form>
--- vue/vue_inscription.php
+++ vue/vue_inscription.php
@@ -1,10 +1,12 @@
- <form action="./index.php" method="post">
+
+ <form action="./index.php" method="post" class = "form">
+ 		 <H1 style="text-align: center;">Inscription</H1>
         <p>
-        <label for="NomUtilisateur">Nom d'utilisateur</label> : <input type="text" name="NomUtilisateur" id="NomUtilisateur" /><br />
-        <label for="Email">Email</label> :  <input type="Email" name="Email" id="Email" /><br />
-        <label for="Mdp">Mot de passe</label> :  <input type="text" name="Mdp" id="Mdp" /><br />
-        <label for="verif_pass">Veuillez retaper votre mot de passe </label> :  <input type="text" name="verif_pass" id="verif_pass" /><br />
-        <label for="Identifiant">Identifiant</label> :  <input type="text" name="Identifiant" id="Identifiant" /><br />
+        <label for="NomUtilisateur">Nom d'utilisateur</label> : </br><input type="text" name="NomUtilisateur" id="NomUtilisateur" /><br />
+        <label for="Email">Email</label> : </br> <input type="Email" name="Email" id="Email" /><br />
+        <label for="Mdp">Mot de passe</label> : </br> <input type="text" name="Mdp" id="Mdp" /><br />
+        <label for="verif_pass">Veuillez retaper votre mot de passe </label> : </br> <input type="text" name="verif_pass" id="verif_pass" /><br />
+        <label for="Identifiant">Identifiant</label> : </br> <input type="text" name="Identifiant" id="Identifiant" /><br />
 
         <input type="submit" value="inscription" name="inscription" />
 	</p>
