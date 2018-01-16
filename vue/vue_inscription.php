bcbc14bb9457a94d06b9820f1f69d3f161345849
Guillaume <fuditphuguillaume@gmail.com>
2018-01-16 15:21:07 +0100

    da

--- vue/vue_inscription.php
+++ vue/vue_inscription.php
@@ -2,18 +2,18 @@
  <form action="index.php" method="post" class = "form">
  		 <H1 class="titre-centré">Inscription</H1>
         <p>
-        <label for="NomUtilisateur">Nom d'utilisateur</label> : </br><input type="text" name="NomUtilisateur" id="NomUtilisateur" required/><br />
-        <label for="Email">Email</label> : </br> <input type="Email" name="Email" id="Email" required /><br />
-        <label for="Mdp">Mot de passe</label> : </br> <input type="password" name="Mdp" id="Mdp" required /><br />
-        <label for="verif_pass">Veuillez retaper votre mot de passe </label> : </br> <input type="text" name="verif_pass" id="verif_pass" required />
+        <label for="NomUtilisateur">Nom d'utilisateur</label> : </br><input type="text" name="NomUtilisateur" id="Form_inscription" required/><br />
+        <label for="Email">Email</label> : </br> <input type="Email" name="Email" id="Form_inscription" required /><br />
+        <label for="Mdp">Mot de passe</label> : </br> <input type="password" name="Mdp" id="Form_inscription" required /><br />
+        <label for="verif_pass">Veuillez retaper votre mot de passe </label> : </br> <input type="password" name="verif_pass" id="Form_inscription" required />
         <div id="bad_mdp" class ="error">
         Les deux mots de passes sont différents</div>
       
-        <label for="Identifiant">Identifiant</label> : </br> <input type="text" name="Identifiant" id="Identifiant" required />
+        <label for="Identifiant">Identifiant</label> : </br> <input type="text" name="Identifiant" id="Form_inscription" required />
         </br>
         <div id="bad_id" class ="error">
          Votre Identifiant est déjà utilisé
         </div>
-        <input type="submit" value="INSCRIPTION" name="inscription" />
+        <input id="Form_inscription_submit" type="submit" value="INSCRIPTION" name="inscription" />
 	</p>
 </form>
