2cea874b438232683c2bf76d2c93852fafac177a
Ulrike <ulrike.kulzer@uni-ulm.de>
2018-01-24 16:23:21 +0100

    doc plus corrigé de texte

--- vue/footer.php
+++ vue/footer.php
@@ -3,7 +3,8 @@
 </head> 
 </body>
 <div id ="footer" >
-
-  <p>Copyright 2018.          </br>  FUDITPHU Yan-Guillaume, DANG Hung Hoang, KULZER Ulrike    </br>         Nous contacter: yanguillaume.fuditphu@edu.esiee.fr</p>
+  <p>Copyright 2018</br>
+  FUDITPHU Yan-Guillaume, DANG Hung Hoang, KULZER Ulrike</br>
+  Nous contacter: yanguillaume.fuditphu@edu.esiee.fr</p>
   </div>
 </html>
