3728e2d2af229bc2f54feda8cc086ec5a3681c96
Guillaume <fuditphuguillaume@gmail.com>
2018-01-26 00:10:44 +0100

    da

--- Rika/vue_Rika/deconnexion.php
+++
@@ -1,7 +0,0 @@
-<?php
-//pour déconnecter la session
-session_start();
-session_destroy();
-$redir = !empty($_SERVER['HTTP_REFERER']) ? $_SERVER['HTTP_REFERER'] : 'index.php';
-header('Location: '.$redir);
-exit();
